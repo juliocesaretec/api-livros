09a15d928eb8daa04da0f8f632fe12e30332ad78
juliocesaretec <161508817+juliocesaretec@users.noreply.github.com>
2026-09-25 22:45:28 -0300

    feat: permite acesso do frontend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -13,6 +14,17 @@ app = FastAPI(
     title="API de Livros",
     version="1.0.0",
     description="API didática para gerenciamento de livros.",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type"],
 )
 
 
